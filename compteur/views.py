2f03a92cab4e9455b5bcf2673a76296854b92528
patjouk <lucie.daeye@gmail.com>
2015-06-26 14:41:57 +0200

    Let's play with variable :)

--- compteur/views.py
+++ compteur/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    # defining the variable
+    number = 6
+    # passing the variable to the view
+    return render(request, 'index.html', {
+        'number': number
+    })
