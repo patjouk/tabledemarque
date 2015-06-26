9cddd25f9c0e9a5c7b58d397388fe2138985d2af
patjouk <lucie.daeye@gmail.com>
2015-06-26 15:33:17 +0200

    Creation of a simple model called "Jeux".

--- compteur/models.py
+++ compteur/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Jeux(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    jeux = models.SlugField(unique=True)
